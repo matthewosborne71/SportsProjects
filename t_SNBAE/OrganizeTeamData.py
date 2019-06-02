c47657f9ee5c89a1f1fbcba2d8463b24fd8d86e4
Matthew Osborne <matthewosborne71@gmail.com>
2019-06-02 10:33:18 -0400

    Updated OrganizeTeamData and PCA

--- t_SNBAE/OrganizeTeamData.py
+++ t_SNBAE/OrganizeTeamData.py
@@ -6,10 +6,11 @@ import seaborn as sns
 TeamShots = pd.read_csv("NBAShots_2013_2019.csv")
 
 # Add a new variable that combines the two shot zone variables into one
-TeamShots['ZONE_DETAIL'] = TeamShots[['SHOT_ZONE_BASIC','SHOT_ZONE_AREA']].apply(lambda x: ','.join(x), axis = 1)
+TeamShots['ZONE_DETAIL'] = TeamShots[['SHOT_ZONE_BASIC','SHOT_ZONE_AREA']].apply(lambda x: '-'.join(x).replace(" ", "_"), axis = 1)
 # Combine both Backcourt zones into one
-TeamShots.loc[TeamShots.ZONE_DETAIL == 'Backcourt,Back Court(BC)','ZONE_DETAIL'] = 'Backcourt'
-TeamShots.loc[TeamShots.ZONE_DETAIL == 'Above the Break 3,Back Court(BC)','ZONE_DETAIL'] = 'Backcourt'
+TeamShots.loc[TeamShots.ZONE_DETAIL == 'Backcourt-Back_Court(BC)','ZONE_DETAIL'] = 'Backcourt'
+TeamShots.loc[TeamShots.ZONE_DETAIL == 'Above_the_Break_3-Back_Court(BC)','ZONE_DETAIL'] = 'Backcourt'
+
 
 # This will plot and save a picture of all the zones created in ZONE_DETAIL.
 # The plot will make it easier to visualize where all of the ZONE_DETAIL zones
@@ -27,33 +28,15 @@ plt.savefig("ZONE_DETAIL_Court_Location.png")
 plt.close()
 
 # Isolate just the team, season, ZONE_DETAIL, and make/miss
-Shot_by_Zone = TeamShots[['TEAM_NAME','Season','ZONE_DETAIL','SHOT_MADE_FLAG']]
+Shot_by_Zone = TeamShots[['TEAM_NAME','Season','ZONE_DETAIL','SHOT_MADE_FLAG']].copy()
 
 # Create Dummy Variables
 Dummies = pd.get_dummies(Shot_by_Zone['ZONE_DETAIL'])
 
-# Helps us know which zone is coded as ZONE_i, i = 0,...,15
-ZoneKey = {}
-i = 0
 
-# Helps with aesthetic so that zones are arranged in numerical order
-def sort_key(x):
-    if len(x) == 6:
-        return int(x[5])
-    else:
-        return int(x[5:])
-
-
-for col in Dummies.columns:
-    ZoneKey[col] = 'ZONE_' + str(i)
-    i = i + 1
-del i
-
-Dummies = Dummies.rename(columns = ZoneKey)
 
 # These will be used to get our columns later
 Columns = list(Dummies.columns)
-Columns.sort(key = sort_key)
 
 # Add dummy variables to our dataframe
 for col in Columns:
@@ -74,21 +57,36 @@ for col in Columns:
     SHOTS_BY_ZONE[col + '_MADE'] = Shot_by_Zone.loc[Shot_by_Zone[col] == 1,].groupby(['TEAM_NAME','Season']).sum()['SHOT_MADE_FLAG']
 
     # Now calculate what percentage of a teams shots were in that zone
-    SHOTS_BY_ZONE[col + '_PERC_OF_TOTAL_SHOTS'] = SHOTS_BY_ZONE[col + '_ATT']/SHOTS_BY_ZONE['TOTAL_SHOTS']
+    SHOTS_BY_ZONE[col + '_PERC_TOT'] = SHOTS_BY_ZONE[col + '_ATT']/SHOTS_BY_ZONE['TOTAL_SHOTS']
     # Now calculate what percentage of shots taken in that zone were made
     SHOTS_BY_ZONE[col + '_PERC_MADE'] = SHOTS_BY_ZONE[col + '_MADE']/SHOTS_BY_ZONE[col + "_ATT"]
 
     # These are the columns we want in our final df
-    Columns_Wanted.extend([col + '_PERC_OF_TOTAL_SHOTS',col + '_PERC_MADE'])
+    Columns_Wanted.extend([col + '_PERC_TOT',col + '_PERC_MADE'])
 
-# Record the zone key so we know what zones are coded as ZONE_i
-f = open("ZONE_KEY.csv","w+")
-f.write("ZONE_DETAIL,CODE\n")
-for key in ZoneKey.keys():
-    f.write(key + "," + ZoneKey[key] + "\n")
 
-f.close()
 
-# Save the df with the desired columns
-SHOTS_BY_ZONE = SHOTS_BY_ZONE[Columns_Wanted]
+# Save only the columns we want
+SHOTS_BY_ZONE = SHOTS_BY_ZONE[Columns_Wanted].copy()
+
+# Break down by 3, mid, and close range
+col_3 = []
+col_mid = []
+col_close = []
+
+for c in Columns_Wanted:
+    if '_PERC_TOT' in c:
+        if ('3' in c) or (c == 'Backcourt'):
+            col_3.append(c)
+        elif 'Mid' in c:
+            col_mid.append(c)
+        else:
+            col_close.append(c)
+
+SHOTS_BY_ZONE['3_PERC_OF_TOT'] = SHOTS_BY_ZONE[col_3].sum(axis = 1)
+SHOTS_BY_ZONE['MID_PERC_OF_TOT'] = SHOTS_BY_ZONE[col_mid].sum(axis = 1)
+SHOTS_BY_ZONE['CLOSE_PERC_OF_TOT'] = SHOTS_BY_ZONE[col_close].sum(axis = 1)
+
+
+
 SHOTS_BY_ZONE.to_csv("Team_Shots_by_Zone.csv")
--- t_SNBAE/PCA.py
+++ t_SNBAE/PCA.py
@@ -4,8 +4,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-team_by_zone = pd.read_cs("Team_Shots_by_Zone.csv")
-variables = team_by_zone.loc[0:len(team_by_zone),team_by_zone[team_by_zone.columns[2:]]]
+team_by_zone = pd.read_csv("Team_Shots_by_Zone.csv")
 
-pca = PCA(n_components = len(variables.columns))
-pca.fit(team_by_zone)
+variables = [c for c in team_by_zone.columns if '_PERC_TOT' in c]
+X = team_by_zone[variables].copy()
+
+pca = PCA(n_components = len(variables))
+pca.fit(X)
+
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.show()
+
+components = pd.DataFrame({'feature': variables})
+
+for i in range(len(variables)):
+    components['component_' + str(i+1)] = pca.components_[i,:]
+
+components.to_csv("team_pca_components.csv", index = False)
+
+
+fit = pca.transform(X)
+
+
+team_by_zone['pca_1'] = fit[:,0]
+team_by_zone['pca_2'] = fit[:,1]
+
+team_by_zone.to_csv("Team_Shots_by_Zone.csv", index = False)
